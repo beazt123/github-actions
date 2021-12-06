b87683fa6d9ec535348ff683df70c92746e72531
beazt123 <lohzhixuan@live.com>
2021-12-06 14:39:17 +0800

    Add bad code to hello class

--- hello.java
+++ hello.java
@@ -1,9 +1,23 @@
 
 public class hello {
     public static void main(String[] args) {
-        System.out.println("Hello world")
+        System.out.println("Hello world");
     },
-    public int doSomething() {
+    public int                                                                                                                doSomething() {
         return 1;
+    },
+    public int                                                                                                                doSomethingElse() {
+        return 1;
+    },
+    public int                                                                                                                doSomethingElseAgain() {
+        return 1;
+    },
+    public void method2(int input) {
+        if (true) {
+            System.out.println("method 2 if");
+        }
+        else {
+            System.out.println("method 2 else");
+        }
     }
 }
